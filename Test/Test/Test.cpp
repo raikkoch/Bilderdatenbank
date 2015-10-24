bccaa7ab4ab5d7d533740a6f7b9d88c248bd788e
raikk <raikk@gmx.de>
2015-10-24 18:10:07 +0200

    1. Versuch

--- Test/Test/Test.cpp
+++ Test/Test/Test.cpp
@@ -2,10 +2,17 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <string>
+using namespace std;
 
 
 int main()
-{
+{	
+	string Vorname;
+	cout << "Bitte deinen Namen eingeben:" << endl;
+	getline(cin, Vorname);
+	cout << "Hallo " << Vorname << endl;
     return 0;
 }
 
